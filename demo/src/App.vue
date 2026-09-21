3e51c9adec834a58f0811de862e1a98b1838b606
GnanaGanesh Anem <backpropbrigade@gmail.com>
2026-09-21 22:14:53 +0530

    Add files via upload

---
+++ demo/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-[#0b0e14] text-slate-100 flex flex-col font-sans">
+    <Header />
+    <main class="flex-1 px-4 py-4 max-w-[1600px] w-full mx-auto">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
